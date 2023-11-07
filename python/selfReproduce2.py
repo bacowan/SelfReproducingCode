ab92fc68e7394ce82b1f5058c663f3c6018fb391
bacowan <bacowan@ualberta.ca>
2023-11-07 19:08:57 +0900

    new python implementation using mostly just print

--- python/selfReproduce2.py
+++ python/selfReproduce2.py
@@ -1,1 +1,6 @@
-print([])
+string = r'print("string = r", end="")\nprint("\'", end="")\nprint(string, end="")\nprint("\'")\nprint(string.replace("\\n","\n", 4), end="")'
+print("string = r", end="")
+print("\'", end="")
+print(string, end="")
+print("\'")
+print(string.replace("\\n","\n", 4), end="")
