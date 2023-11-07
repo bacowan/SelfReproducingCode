5f19da18ea71778d4d4c5ed1938ab20f290f7f64
bacowan <bacowan@ualberta.ca>
2023-11-07 19:22:12 +0900

    cleanup and notes

--- javascript/selfReproduce.js
+++ javascript/selfReproduce.js
@@ -2,37 +2,37 @@ quote = "'"
 str = null
 printQueue('str = null')
 printQueue('function printQueue(x) {')
-printQueue('if (str === null) {')
-printQueue('str = []')
-printQueue('console.log(`quote = "${quote}"`)')
-printQueue('console.log("str = null")')
-printQueue('}')
-printQueue('else {')
-printQueue('str.push(x)')
-printQueue('}')
-printQueue('console.log(`printQueue(${quote}${x}${quote})`)')
+printQueue('    if (str === null) {')
+printQueue('        str = []')
+printQueue('        console.log(`quote = "${quote}"`)')
+printQueue('        console.log("str = null")')
+printQueue('    }')
+printQueue('    else {')
+printQueue('        str.push(x)')
+printQueue('    }')
+printQueue('    console.log(`printQueue(${quote}${x}${quote})`)')
 printQueue('}')
 printQueue('function end() {')
-printQueue('console.log("end()")')
-printQueue('for (x = 0; x < str.length; x++) {')
-printQueue('console.log(str[x])')
-printQueue('}')
+printQueue('    console.log("end()")')
+printQueue('    for (x = 0; x < str.length; x++) {')
+printQueue('        console.log(str[x])')
+printQueue('    }')
 printQueue('}')
 end()
 function printQueue(x) {
-if (str === null) {
-str = []
-console.log(`quote = "${quote}"`)
-console.log("str = null")
-}
-else {
-str.push(x)
-}
-console.log(`printQueue(${quote}${x}${quote})`)
+    if (str === null) {
+        str = []
+        console.log(`quote = "${quote}"`)
+        console.log("str = null")
+    }
+    else {
+        str.push(x)
+    }
+    console.log(`printQueue(${quote}${x}${quote})`)
 }
 function end() {
-console.log("end()")
-for (x = 0; x < str.length; x++) {
-console.log(str[x])
-}
+    console.log("end()")
+    for (x = 0; x < str.length; x++) {
+        console.log(str[x])
+    }
 }
